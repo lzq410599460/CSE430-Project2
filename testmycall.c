26821c9cdfa76f5a8ccf4a948d46560a681bf191
mesmesgit <Mark.Strickland@asu.edu>
2016-02-07 15:51:40 -0700

    revised to first working version

--- testmycall.c
+++ testmycall.c
@@ -1,14 +1,38 @@
 #include <linux/unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define __NR_my_syscall 359
 
 int main()
 {
-	char buffer[512];
-	int n = 0;
-	n = syscall(__NR_my_syscall, 0, sizeof(buffer), buffer);
+	// declare return value for system call
+	int ret = 0;
+	// declare buffer to hold result of system call
+	char * buffer;
+
+	// set the size of the buffer in bytes
+	int n = 4000;
+
+	// allocate memory for buffer to hold result
+	if( ( buffer = (char *) malloc(n+1) ) == NULL ) 
+		perror( "Error allocating buffer\n" );
+
+	// perform system call, storing return value in ret
+	// result of system call will be stored in buffer
+	ret = syscall(__NR_my_syscall, 0, sizeof(buffer), buffer);
+
+	// print the results of the system call - contents of buffer
 	printf("buffer = %s\n", buffer);
-	printf("n = %d\n", n);
+
+	// print the return value from system call
+	// this should have the number of bytes not copied to userspace
+	printf("ret = %d\n", ret);
+
+	// de-allocate the memory allocated for the buffer
+	free(buffer);
+
+	// return 0 upon success
 	return 0;
 }
+
 

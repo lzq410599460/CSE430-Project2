44b974925fc2a6f3151ebc468ff3d6cf677c722d
lzq410599460 <zhiqianl2012@gmail.com>
2016-02-29 17:26:14 -0700

    Update sema.c

--- sema.c
+++ sema.c
@@ -1,70 +1,101 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<semaphore.h>
+//This is just a basic function module for semaphore, will improve.
+
+#include <linux/module.h>		/* Needed by all modules */
+#include <linux/kernel.h>		/* Needed for KERN_INFO */
+#include <linux/init.h>			/* Needed for the macros */
+#include <linux/sched.h>	// allows access to task_struct
+#include <linux/delay.h>	// allows use of msleep()
+#include <linux/kthread.h>	// attempt to avoid implicit macro errors
+#include <linux/semaphore.h>	// attempt to avoid down_int errors
+#include <linux/list.h>		// used for struct list_head
+#include <linux/signal.h>	// used for do_send_sig_info
+#include <linux/slab.h>		// added for kmalloc
+#include <linux/kfifo.h>	// added for kfifo
+#include <linux/pid_namespace.h>	// added for find_task_by_vpid
+#include <linux/pid.h>		// added for PIDTYPE_PID
 
 
-int PID=0;
-int num_children;
-int p2k;
-pthread_t pro_thread,con_thread;
-pthread_mutex_t lock;
-sem_t mysem;
+// declare global variables to allow start and stop kthread
+struct task_struct *task_prod;	// producer thread
+struct task_struct *task_cons;	// consumer thread
 
-void* producer(void* _)
+MODULE_LICENSE("GPL");
+
+int count=4;     //this count control the loop between producer and consumer
+int p2k;	//store the PID for fork bomb
+struct semaphore write;		//semaphore to sync
+struct semaphore read;
+
+
+
+
+int producer(void *data) 
 {
-
-printf("\nProducer\n");
-
-
-while(1 && PID<10)
-{
-
-		//I just do some simulation about process
-		PID=1+PID;
-		num_children=PID*3;
-		printf("PID:%d  Children process:%d\n",PID,num_children);
-		
-	if (num_children=6)  //Assume we regard 6 children process's parent as fork bomb
+	int j=0;			//j variable is just for test we will use for_each_task() in the while loop with -
+	down(&read);			//while (1) to keep it going
+	while(j<10)
 	{
-	pthread_mutex_unlock(&lock);
-	p2k=num_children/3;
-	//num=0;
+		//read_lock(&tasklist_lock)  not tested yet (in linux/sched.h ln317 )
+		printk(KERN_INFO "PID : %d  children: %d\n",j,-(j-5)*(j-5)+30);
+		j++;
+		if(-(j-5)*(j-5)+30>29)		//assume 29 is threshold of fork bomb
+		{
+			p2k=j;
+			up(&write);
+			
+		}
+		//read_unlock(&tasklist_lock)
+		//put sleep time here 
 	}
-//sleep
+return 0;
 }
 
-
-
-}
-void* consumer(void* _)
-{printf("\nConsumer\n");
-	int count=0;
-	
-	
-
-	pthread_mutex_lock(&lock);
-
-	
-		printf("Process to kill is: %d\n",p2k);
-		
-	
-	//pthread_mutex_unlock(&lock);
-	
-
-
-
-}
-
-int main()
+int consumer (void *data)
 {
-	pthread_mutex_init(&lock,NULL);pthread_mutex_lock(&lock);
-	pthread_create(&pro_thread,NULL,producer,NULL);
-	pthread_create(&con_thread,NULL,consumer,NULL);
+	while(1)
+	{
+		down(&write);
+		printk(KERN_INFO "\n====Process killed ID :%d====\n",p2k);	//kill the fork bomb here
+		up(&read);
+	}
+	return 0;
+}
 
-	pthread_exit(NULL);
+static int __init start(void)
+{
+	sema_init(&write,0);
+	sema_init(&read,1);
 
-return 1;
+	printk(KERN_INFO "\n------STARTING------\n");
+
+	while(count>0)			//using while(1) to keep it going forever
+	{
+		task_prod = kthread_run(&consumer,NULL,"consumer");
+		printk(KERN_INFO "\n------Consuming------\n");
+		task_prod = kthread_run(&producer,NULL,"producer");
+		printk(KERN_INFO "\n------Producing------\n");
+
+		count--;
+	}
+
+return 0;
+}
+
+static void __exit exit(void)
+{
+//int pstopflag = -1;
+//int cstopflag = -1;
+
+	printk(KERN_INFO "-- stopping defuse-mod producer thread --\n");
+//pstopflag = kthread_stop(task_prod);
+
+	printk(KERN_INFO "-- stopping defuse-mod consumer thread --\n");
+//cstopflag = kthread_stop(task_cons);
+
+	printk(KERN_INFO "\n------Exiting------\n");
 
 
 }
 
+module_init(start);
+module_exit(exit);
